32834b6305d3f94906c69af44fcfa26a5e9f61e9
Serkan-Ucakcioglu <serkanuckc123@gmail.com>
2022-08-26 01:26:38 +0300

    city add

--- src/Pages/CityDetail.jsx
+++ src/Pages/CityDetail.jsx
@@ -1,28 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 function CityDetail() {
-    const { cityname } = useParams();
-    const [data, setData] = useState([]);
+  const { cityname } = useParams();
+  const [data, setData] = useState([]);
 
-    const getDetail = () => {
-        fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=" +
-            cityname +
-            "&appid=5ea20c34427b9975a24ca2c9d4b64486"
-        )
-            .then((res) => res.json())
-            .then((dat) => setData(dat));
-    };
-    useEffect(() => {
-        getDetail();
-    }, []);
-    console.log(data);
-    return (
-        <div>
-            <div>{cityname}</div>
-            {data.base}
-        </div>
-    );
+  const getDetail = () => {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        cityname +
+        "&appid=5ea20c34427b9975a24ca2c9d4b64486"
+    )
+      .then((res) => res.json())
+      .then((dat) => setData(dat));
+  };
+  useEffect(() => {
+    getDetail();
+  }, []);
+  console.log(data);
+  return (
+    <div>
+      <div>{cityname}</div>
+      {data.base}
+    </div>
+  );
 }
 
 export default CityDetail;
