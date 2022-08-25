074331628dbadfbeb2e0c3b8e672e9ccc1aa62a5
Serkan-Ucakcioglu <serkanuckc123@gmail.com>
2022-08-26 01:29:36 +0300

    fix

--- src/Pages/CityDetail.jsx
+++ src/Pages/CityDetail.jsx
@@ -4,18 +4,16 @@ function CityDetail() {
   const { cityname } = useParams();
   const [data, setData] = useState([]);
 
-  const getDetail = () => {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        cityname +
-        "&appid=5ea20c34427b9975a24ca2c9d4b64486"
-    )
-      .then((res) => res.json())
-      .then((dat) => setData(dat));
-  };
+
   useEffect(() => {
-    getDetail();
-  }, []);
+    fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          cityname +
+          "&appid=5ea20c34427b9975a24ca2c9d4b64486"
+      )
+        .then((res) => res.json())
+        .then((dat) => setData(dat));
+  }, [cityname]);
   console.log(data);
   return (
     <div>
