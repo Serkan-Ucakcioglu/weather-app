02821eb23d40b3c1f340c68f2ca4c19bec30050f
Serkan-Ucakcioglu <serkanuckc123@gmail.com>
2022-08-26 14:17:20 +0300

    text fix

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -63,7 +63,7 @@ export default function Dashboard() {
                   key={city.name}
                   className="focus:outline-none  cursor-pointer text-center p-3 text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
                 >
-                  {city.id} {city.name}
+                {city.name}
                 </div>
               </NavLink>
             );
